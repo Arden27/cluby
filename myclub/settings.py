3d50628153c66e5d3eb16365b22ac296517d0146
Artem Furman <artemfurman@gmail.com>
2023-05-22 22:41:00 +0200

    settings changed

--- myclub/settings.py
+++ myclub/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
-from decouple import config
+#import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -56,7 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    #'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = "myclub.urls"
@@ -128,12 +126,10 @@ STTICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompresseManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompresseManifestStaticFilesStorage'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-django_heroku.settings(locals())
